993ce67f9e1cad186abecc2741895aeb530fe4ab
yanzhenglong <yanzhenglong@bytedance.com>
2022-12-09 16:45:10 +0800

    update

--- pyspark/basic_operation.py
+++ pyspark/basic_operation.py
@@ -3,24 +3,25 @@ from pyspark.sql import functions as F
 from pyspark.sql.types import IntegerType,ArrayType
 from pyspark.sql.functions import udf as udf
 
+
+
+#Demo1: --------------------------------------------------------------------------
+# df.groupBy后，使用agg函数
 data = [
     {"aid":11, "type":"t1", "pctr": 0.2, "time": "2022-11-06 12:02"}, 
     {"aid":22, "type":"t2", "pctr": 0.25, "time": "2022-11-06 12:02"},
     {"aid":33, "type":"t1", "pctr": 0.6, "time": "2022-11-06 10:02"},
     {"aid":66, "type":"t2", "pctr": 0.6, "time": "2022-11-06 11:02"}
 ]
-
 df = spark.createDataFrame(data)
-
-df.printSchema() #打印schema
+df.printSchema()
 
 #求平均
 stat = df.groupBy("type").agg(F.mean("pctr"))
 
 
-#collesce的用法
-
-#Demo3: F.expr将 json object 解开
+#Demo2:----------------------------------------------------------------------------
+# F.expr将 json object 解开
 data = [
     {"aid":11, "obj1": {"oid": '1111', "slot_id":2}},
     {"aid":66, "obj1": {"oid": '6666', "slot_id":9}},
@@ -29,13 +30,11 @@ data = [
     {"aid":88, "obj1": {"oid": '8888', "slot_id":1}},
 ]
 df = spark.createDataFrame(data)
-
 df2 = df.withColumn("obj_id", F.expr('obj1.oid').cast('string'))
 
 
-
-
-#Demo4: F.regexp_extract使用正则表达式,提取部分字符串
+#Demo3: --------------------------------------------------------------------------
+# F.regexp_extract使用正则表达式,提取部分字符串
 data = [
     {"aid":11, "type":"t1", "pctr": 0.2, "ppsABTag": "aaa#perfmThr_Huoshan02#aaaaa"},
     {"aid":22, "type":"t2", "pctr": 0.25, "ppsABTag": "bbb#perfmThr_Huoshan01#bbbb"},
@@ -44,6 +43,15 @@ data = [
 ]
 df = spark.createDataFrame(data)
 df2 = df.withColumn('ab_version', F.regexp_extract('ppsABTag', r'(perfmThr.*?)(?=#)', 1))
+
+
+
+
+
+
+
+
+
 
 
 
--- pyspark/udf.py
+++ pyspark/udf.py
@@ -1,23 +1,22 @@
 #coding=utf-8
 #读取user_profile数据，然后统计每个slot的覆盖率
 
+from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType,ArrayType,StringType,LongType
+from pyspark.sql.functions import udf as udf
+
+#Demo1:-------------------------------------------------------------------------------------
+#对df某一列操作，提取出slot_id，并
 data = [
     {"aid":11, "fids":"725900317002156569 72590031700215650"}, 
     {"aid":22, "fids":"725900317002156559 725900317002156558 725900317002156559"},
     {"aid":33, "fids":"625900317002156569 785900317002156569"}
 ]
-
 df = spark.createDataFrame(data)
-
 df.printSchema() #打印schema
-
-from pyspark.sql import functions as F
-from pyspark.sql.types import IntegerType,ArrayType,StringType
-from pyspark.sql.functions import udf as udf
 
 df2 = df.select(F.split('fids',' ').alias('fid_list'))
 df3 = df2.withColumn("fid_tmp", df2.fid_list.cast("array<long>"))
-
 
 def func_udf(col):
 	res = []
@@ -32,16 +31,14 @@ def func_udf2(col):
 	return list(res)
 
 tmp_udf = udf(lambda x:func_udf2(x), ArrayType(IntegerType()))
-rdf = df3.select("fid_tmp", tmp_udf("fid_tmp")).withColumnRenamed("<lambda>(fid_tmp)", "slots")
-
+rdf = df3.withColumn("slots", tmp_udf("fid_tmp"))
 
 from pyspark.sql.functions import collect_set
 rdf.agg(collect_set('slots')).collect()
 
-#Demo1:
+#Demo2:-----------------------------------------------------------------------------
 #针对某一列使用udf提取字符串。ppsABTag='111#222#perfmThr_Huoshan01#888'，
 #从这里面提取出来perfmThr_作为新的一列ab_version
-#-----------------------------------------------------------------------------
 data = [
     {"aid":11, "type":"t1", "pctr": 0.2, "ppsABTag": "aaa#perfmThr_Huoshan02#aaaaa"},
     {"aid":22, "type":"t2", "pctr": 0.25, "ppsABTag": "bbb#perfmThr_Huoshan01#bbbb"},
@@ -65,9 +62,8 @@ def process_ab_version_from_ppsABTag(df):
 df2 = process_ab_version_from_ppsABTag(df)
 
 
-#Demo2:
+#Demo3:-------------------------------------------------------------------------
 #df.groupBy后，在agg中使用udf函数
-#-------------------------------------------------------------------------
 data = [
     {"aid":11, "value":"a"},
     {"aid":11, "value":"b"},
@@ -90,9 +86,9 @@ find_a_udf = F.udf(find_a, T.IntegerType())
 df.groupBy('aid').agg(find_a_udf(F.collect_list('value')).alias('a_count')).show()
 
 
-#Demo3:
-#udf作用在2列上
-#------------------------------------------------------------------------
+#Demo4: ------------------------------------------------------------------------
+#df.groupBy后，udf作用在2列上
+
 data = [
     {"aid":11, "col_a":"t1", "col_b": 1},
     {"aid":22, "col_a":"t2", "col_b": 5},
@@ -111,9 +107,33 @@ udf_func_two_cols = udf(lambda x,y: func_two_cols(x, y), StringType())
 df.groupBy('aid').agg(udf_func_two_cols(F.collect_list('col_a'),F.collect_list('col_b')).alias('mix_cols')).show()
 
 
+#Demo5: -----------------------------------------------------------------------------------------------
+#原始df中fids是一个string，空格隔开，用空格切分，每个fid转成Long类型，然后提取slot=36的fid
 
+data = [
+    {"aid":11, "fids":"725900317002156569 72590031700215650"},
+    {"aid":22, "fids":"725900317002156559 725900317002156558 725900317002156559"},
+    {"aid":33, "fids":"625900317002156569 785900317002156569"}
+]
+df = spark.createDataFrame(data)
 
+def func_get_ssp_id(col):
+    res = -1
+    for fid in col:
+        slot_id  = fid >> 54
+        if slot_id == 36:
+            res = fid
+            return res
+    return res
+udf_func_sspid = F.udf(lambda x:func_get_ssp_id(x), LongType())
 
+def get_ssp_fid(df):
+    df2 = df.withColumn('fid_list_tmp', F.split('fids', ' '))
+    df3 = df2.withColumn('fid_list', df2.fid_list_tmp.cast('array<long>'))
+    df4 = df3.withColumn('ssp_id_fid', udf_func_sspid('fid_list'))
+    return df4
+
+#-------------------------------------------------------------------------------------------
 
 
 
