3bf5b40365eb7ec256197d5d5fd90a451e79bd10
yanzhenglong <yanzhenglong@bytedance.com>
2023-09-20 16:06:00 +0800

    update

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -12,8 +12,12 @@ kubectl describe pod xxxx(pod_name)
 
 #删除某个pod
 kubectl delete pod xxx(pod_name)
+kubectl get pod | grep key_word | awk '{print $1}' | xargs kubectl delete pod  #批量删除
 
 #修改pod配置，会重启
 kubectl edit deploy xxx(pod_name)
-
+在template:
+    metadata:
+        annotations:
+            force-update: "1"
 
